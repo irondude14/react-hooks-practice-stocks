28d41ff47050a7fd2d4b4973e313c259fa81bf37
Vladyslav <vkadyrov@protonmail.com>
2022-12-11 20:20:58 -0500

    commit tree aba53ea7f1499383c7b1d2780ed1759f06708240

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 
 function Stock() {
-  return (
-    <div>
-      <div className="card">
-        <div className="card-body">
-          <h5 className="card-title">{"Compant Name"}</h5>
-          <p className="card-text">{"Stock Price"}</p>
+  const [stocks, setStocks] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/stocks')
+      .then((r) => r.json())
+      .then((data) => setStocks(data));
+  }, []);
+
+  const displayStockContainer = stocks.map((stock) => {
+    return (
+      <div className='card' key={stock.id}>
+        <div className='card-body'>
+          <h5 className='card-title'>{stock.name}</h5>
+          <p className='card-text'>{stock.price}</p>
         </div>
       </div>
-    </div>
-  );
+    );
+  });
+
+  return <div>{displayStockContainer}</div>;
 }
 export default Stock;
--- src/components/StockContainer.js
+++ src/components/StockContainer.js
@@ -1,11 +1,11 @@
-import React from "react";
-import Stock from "./Stock";
+import React from 'react';
+import Stock from './Stock';
 
 function StockContainer() {
   return (
     <div>
       <h2>Stocks</h2>
-      {/* render stock list here*/}
+      <Stock />
     </div>
   );
 }
